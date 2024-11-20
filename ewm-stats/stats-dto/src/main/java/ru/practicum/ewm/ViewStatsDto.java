d438215cd0fa92588ac825bb469317c12abac4ad
montenok <montenok-d@yandex.ru>
2024-11-20 15:57:14 +0300

    Add modifiers to dto, add lombok annotation to ApiError

--- ewm-stats/stats-dto/src/main/java/ru/practicum/ewm/ViewStatsDto.java
+++ ewm-stats/stats-dto/src/main/java/ru/practicum/ewm/ViewStatsDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ViewStatsDto {
-    String app;
-    String uri;
-    Long hits;
+    private String app;
+    private String uri;
+    private Long hits;
 }
--- ewm-stats/stats-service/src/main/java/ru/practicum/ewm/error/ApiError.java
+++ ewm-stats/stats-service/src/main/java/ru/practicum/ewm/error/ApiError.java
@@ -1,19 +1,15 @@
 package ru.practicum.ewm.error;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ApiError {
     private HttpStatus status;
     private String errorMessage;
     private String terminalMessage;
-
-    public ApiError(HttpStatus status, String errorMessage, String terminalMessage) {
-        this.status = status;
-        this.errorMessage = errorMessage;
-        this.terminalMessage = terminalMessage;
-    }
 }
